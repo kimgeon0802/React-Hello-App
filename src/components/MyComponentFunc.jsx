d6df03ed4df4739ef0d793bdc54de03e43278f3f
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-23 14:08:28 +0900

    useState add

--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //Function 형태의 컴포넌트
 const MyComponentFunc = ({name,children}) => {
+    //useState() Hook 사용
+    const [value, setValue] = useState(0);
+    
     return (
         <div>
             <h2>함수 형태의 컴포넌트</h2>
             <h3>Hello!! {name}</h3>
             {children}
+            <button>증가</button>
+            <button>감소</button>
         </div>
     );
 };
